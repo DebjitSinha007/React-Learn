87e45feb7ce096f5448365bcf2fc9b8e1077b821
Debjit Sinha <shinadebjit007@gmail.com>
2023-10-20 01:29:03 +0530

    Add my old basic react learning projects

---
+++ ReactRouter/src/components/User/User.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { useParams } from "react-router-dom";
+
+const User = () => {
+    const {userId} = useParams()
+  return (
+    <div>User: {userId} </div>
+  )
+}
+
+export default User
